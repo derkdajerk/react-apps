9278341b7dfc5f1652f7d5c9abc1487bdf2a1bfd
Derek Trauner <derektrauner@gmail.com>
2025-04-17 20:04:17 -0700

    Created custom componenet for general class list viewing.

--- class-connect-react-js/src/App.jsx
+++ class-connect-react-js/src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
 import { createClient } from "@supabase/supabase-js";
+import ClassScrollBar from "@/components/classScrollBar.jsx";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? null;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY ?? null;
 
@@ -12,35 +11,35 @@ if (!supabaseUrl || !supabaseKey) {
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-let { data: danceClass, error: error1 } = await supabase
+let { data: danceClassTMILLY, error: error1 } = await supabase
   .from("danceClassStorage")
   .select("classname,instructor,price,time,length")
   .eq("studio_name", "TMILLY")
   .eq("date", "2025-04-14")
   .order("time", { ascending: true });
 
-if (!danceClass) {
+if (!danceClassTMILLY) {
   throw new Error(`Data came back null: ${JSON.stringify(error1)}`);
 }
-let { data: danceClass2, error: error2 } = await supabase
+let { data: danceClassMDC, error: error2 } = await supabase
   .from("danceClassStorage")
   .select("classname,instructor,price,time,length")
   .eq("studio_name", "MDC")
   .eq("date", "2025-04-14")
   .order("time", { ascending: true });
 
-if (!danceClass2) {
+if (!danceClassMDC) {
   throw new Error(`Data came back null: ${JSON.stringify(error2)}`);
 }
 
-let { data: danceClass3, error: error3 } = await supabase
+let { data: danceClassML, error: error3 } = await supabase
   .from("danceClassStorage")
   .select("classname,instructor,price,time,length")
   .eq("studio_name", "ML")
   .eq("date", "2025-04-14")
   .order("time", { ascending: true });
 
-if (!danceClass3) {
+if (!danceClassML) {
   throw new Error(`Data came back null: ${JSON.stringify(error3)}`);
 }
 
@@ -51,7 +50,10 @@ const App = () => {
         <p className="">Hello welcome to ClassConnect!</p>
       </div>
       <div id="navbar" className="text-center">
-        <Button className="bg-red-800" size="xlg">
+        <Button
+          className="bg-gradient-to-t from-gray-800 to-red-800"
+          size="xlg"
+        >
           MDC
         </Button>
         <Button
@@ -71,126 +73,18 @@ const App = () => {
         id="class-content"
         className="flex-1 flex justify-center pt-2 overflow-hidden"
       >
-        <ScrollArea className="w-80 rounded-lg border">
-          <div className="pt-4 pb-4">
-            <h4 className=" text-sm font-medium leading-none text-center">
-              MDC
-            </h4>
-          </div>
-          <Separator className="w-full"></Separator>
-          <div className="p-4">
-            {danceClass2.map((danceClass, index) => (
-              <div key={`${index}`} className="relative mb-2">
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="text-sm text-left">
-                    {danceClass.time[0] + danceClass.time[1] > 12
-                      ? danceClass.time[0] +
-                        danceClass.time[1] -
-                        12 +
-                        danceClass.time[2] +
-                        danceClass.time[3] +
-                        danceClass.time[4] +
-                        " PM"
-                      : danceClass.time[0] +
-                        danceClass.time[1] +
-                        danceClass.time[2] +
-                        danceClass.time[3] +
-                        danceClass.time[4] +
-                        " AM"}
-                  </div>
-                  <div className="text-sm text-right">
-                    {danceClass.classname}
-                  </div>
-                  <div className="text-sm text-left">{danceClass.length}</div>
-                  <div className="text-sm text-right">
-                    {danceClass.instructor}
-                  </div>
-                </div>
-                <Separator className="mt-2" />
-              </div>
-            ))}
-          </div>
-        </ScrollArea>
-        <ScrollArea className="w-80 rounded-lg border">
-          <div className="pt-4 pb-4">
-            <h4 className=" text-sm font-medium leading-none text-center">
-              TMILLY
-            </h4>
-          </div>
-          <Separator className="w-full"></Separator>
-          <div className="p-4">
-            {danceClass.map((danceClass, index) => (
-              <div key={`${index}`} className="relative mb-2">
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="text-sm text-left">
-                    {danceClass.time[0] + danceClass.time[1] > 12
-                      ? danceClass.time[0] +
-                        danceClass.time[1] -
-                        12 +
-                        danceClass.time[2] +
-                        danceClass.time[3] +
-                        danceClass.time[4] +
-                        " PM"
-                      : danceClass.time[0] +
-                        danceClass.time[1] +
-                        danceClass.time[2] +
-                        danceClass.time[3] +
-                        danceClass.time[4] +
-                        " AM"}
-                  </div>
-                  <div className="text-sm text-right">
-                    {danceClass.classname}
-                  </div>
-                  <div className="text-sm text-left">{danceClass.length}</div>
-                  <div className="text-sm text-right">
-                    {danceClass.instructor}
-                  </div>
-                </div>
-                <Separator className="mt-2" />
-              </div>
-            ))}
-          </div>
-        </ScrollArea>
-        <ScrollArea className="w-80 rounded-lg border">
-          <div className="pt-4 pb-4">
-            <h4 className=" text-sm font-medium leading-none text-center">
-              ML
-            </h4>
-          </div>
-          <Separator className="w-full"></Separator>
-          <div className="p-4">
-            {danceClass3.map((danceClass, index) => (
-              <div key={`${index}`} className="relative mb-2">
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="text-sm text-left">
-                    {danceClass.time[0] + danceClass.time[1] > 12
-                      ? danceClass.time[0] +
-                        danceClass.time[1] -
-                        12 +
-                        danceClass.time[2] +
-                        danceClass.time[3] +
-                        danceClass.time[4] +
-                        " PM"
-                      : danceClass.time[0] +
-                        danceClass.time[1] +
-                        danceClass.time[2] +
-                        danceClass.time[3] +
-                        danceClass.time[4] +
-                        " AM"}
-                  </div>
-                  <div className="text-sm text-right">
-                    {danceClass.classname}
-                  </div>
-                  <div className="text-sm text-left">{danceClass.length}</div>
-                  <div className="text-sm text-right">
-                    {danceClass.instructor}
-                  </div>
-                </div>
-                <Separator className="mt-2" />
-              </div>
-            ))}
-          </div>
-        </ScrollArea>
+        <ClassScrollBar
+          studioName="MDC"
+          danceClassList={danceClassMDC}
+        ></ClassScrollBar>
+        <ClassScrollBar
+          studioName="TMILLY"
+          danceClassList={danceClassTMILLY}
+        ></ClassScrollBar>
+        <ClassScrollBar
+          studioName="ML"
+          danceClassList={danceClassML}
+        ></ClassScrollBar>
       </div>
     </main>
   );
---
+++ class-connect-react-js/src/components/ClassScrollBar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "./ui/separator";
+
+const formatTime = (time) => {
+  const hours = parseInt(time.slice(0, 2));
+  return hours > 12
+    ? `${hours - 12}${time.slice(2, 5)} PM`
+    : hours === 12
+    ? `${hours}${time.slice(2, 5)} PM`
+    : hours < 10
+    ? `${hours}${time.slice(2, 5)} AM`
+    : `${hours}${time.slice(2, 5)} AM`;
+};
+
+const ClassScrollBar = ({ studioName, danceClassList }) => {
+  return (
+    <ScrollArea className="w-80 rounded-lg border">
+      <div className="pt-4 pb-4">
+        <h4 className=" text-sm font-medium leading-none text-center">
+          {studioName}
+        </h4>
+      </div>
+      <Separator className="w-full"></Separator>
+      <div className="p-4">
+        {danceClassList.map((danceClass, index) => (
+          <div key={`${index}`} className="relative mb-2">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="text-sm text-left">
+                {formatTime(danceClass.time)}
+              </div>
+              <div className="text-sm text-right">{danceClass.classname}</div>
+              <div className="text-sm text-left">{danceClass.length}</div>
+              <div className="text-sm text-right">{danceClass.instructor}</div>
+            </div>
+            <Separator className="mt-2" />
+          </div>
+        ))}
+      </div>
+    </ScrollArea>
+  );
+};
+
+export default ClassScrollBar;
